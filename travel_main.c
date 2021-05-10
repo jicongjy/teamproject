4961999d1f1f4aa93d6693dd527b975427c0f9a4
bada Jang <74064440+JbadaJ@users.noreply.github.com>
2021-05-10 17:17:07 +0900

    Update travel_main.c

--- travel_main.c
+++ travel_main.c
@@ -6,6 +6,27 @@ int main(){
     while(menu!=0){
         menu=selectMenu();
         
+        if(menu==1){
+            //새글 등록
+        }
+        else if(menu==2){
+            //글 조회
+        }
+        else if(menu==3){
+            //글 삭제
+        }
+        else if(menu==4){
+            //글 수정
+        }
+        else if(menu==5){
+            //파일 저장
+        }
+        else if(menu==6){
+            // 검색
+        }
+        else if(menu==7){
+            //포인트 조회
+        }
     }
     return 0;
 }
