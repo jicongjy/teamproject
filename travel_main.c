2d05c3115c082521d0a7d47de4ce2881affda78b
bada Jang <74064440+JbadaJ@users.noreply.github.com>
2021-05-10 17:37:02 +0900

    Update travel_main.c

--- travel_main.c
+++ travel_main.c
@@ -1,16 +1,25 @@
-#include "team.h"
+#include "travel.h"
 
 
 int main(){
     int menu;
+    int count=0;
+    data *d[100];
+    //포인터에 공간 배정
+    for(int i=0;i<100;i++){
+        d[i]=malloc(sizeof(data));
+    }
+    
     while(menu!=0){
-        menu=selectMenu();
-        
+        menu=SelectMenu();
+
         if(menu==1){
             //새글 등록
+            count+=CreatPost(d[count]);
         }
         else if(menu==2){
             //글 조회
+            listPost(d,count);
         }
         else if(menu==3){
             //글 삭제
@@ -27,6 +36,10 @@ int main(){
         else if(menu==7){
             //포인트 조회
         }
+    }
+    //포인터에 배정된 공간 삭제
+    for(int i=0;i<100;i++){
+        free(d[i]);
     }
     return 0;
 }
